adc5da957ccce78f3a662205adbbe090c6848a45
Alon Niv <oakfang@gmail.com>
2018-10-24 14:57:52 +0300

    Add input capabilities

--- index.js
+++ index.js
@@ -2,6 +2,22 @@ const mongodb = require('mongodb');
 
 module.exports = (_params, log) => ({
   type: 'mongodb',
+  async input({ connectionString, db, collection }, execute) {
+    try {
+      const client = await mongodb.MongoClient.connect(
+        connectionString,
+        { useNewUrlParser: true }
+      );
+      const dbCollection = client.db(db).collection(collection);
+      dbCollection
+        .watch([{ $match: { operationType: 'insert' } }])
+        .on('change', ({ fullDocument }) =>
+          execute(JSON.parse(JSON.stringify(fullDocument)))
+        );
+    } catch (err) {
+      log.fatal('Connection to db failed', err);
+    }
+  },
   output({ connectionString, db, collection }) {
     let dbCollection;
     mongodb.MongoClient.connect(
